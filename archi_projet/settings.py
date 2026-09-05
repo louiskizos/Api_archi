e9ca9c85de99003bfce904b8056787167aee3c9c
louiskizos <louisnyembo2@gmail.com>
2026-09-05 14:11:49 +0200

    faire de depots ok

--- archi_projet/settings.py
+++ archi_projet/settings.py
@@ -163,6 +163,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Le dossier où collectstatic rassemble les fichiers
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
